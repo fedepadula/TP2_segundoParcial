93a30470e160dd8e1ec8441224b885d8b7e14572
fedepadula <fedepadula86@gmail.com>
2021-09-02 19:35:43 -0300

    segundoParcial

---
+++ src/segundoParcial.js
@@ -0,0 +1,22 @@
+setTimeout(function(){ console.log("Mensaje"); }, 7000);
+
+const aPagos = [
+    {id: 1, numero: '00000002', importe : 1000, cliente:'R00001'}, 
+    {id: 2, numero: '00000003', importe : 3000, cliente:'C00002'}, 
+    {id: 3, numero: '00000004', importe : 2000, cliente:'R00002'}, 
+    {id: 4, numero: '00000005', importe : 8000, cliente:'C00001'},
+]
+
+aPagos.forEach(element => {
+    console.log("cliente: " + element.cliente + "\timporte:" + element.importe)
+});
+
+aPagos.sort(function (a, b) {return a.cliente - b.cliente})
+
+console.log(aPagos.filter((a) => a.cliente.startsWith('R')))
+
+console.log(aPagos.map(x => ({id : x.id, importe : (x.importe * 1.3), cliente : x.cliente })))
+
+import fs from 'fs'
+
+fs.appendFileSync( './datos.txt' , "primer dato" )
